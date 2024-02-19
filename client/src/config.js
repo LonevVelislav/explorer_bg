c607b3bcbd41a368692e14120baedcd10d592d5c
Velislav Lonev <lonevvelislav@gmail.com>
2024-02-19 16:09:37 +0200

    make notification work for all car services

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@ const ipHost = 'http://192.168.0.103:3000';
 const webHost = 'https://explorer-bg.onrender.com';
 
 const config = {
-    host: localHost,
+    host: webHost,
 };
 
 export default config;
